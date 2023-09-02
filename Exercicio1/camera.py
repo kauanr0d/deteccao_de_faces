8ddb45dadfc71e4fdacc989d8309cd05bd87b31c
kauanr0d <kauankenney16752@gmail.com>
2023-09-02 19:18:54 -0300

    correcao arquivo

--- Exercicio1/camera.py
+++ Exercicio1/camera.py
@@ -2,8 +2,8 @@ import dlib
 import cv2
 import os
 
-dir = os.path.abspath(os.path.abspath(__file__))
-caminho_detector = '/home/kauan/Área de Trabalho/PythonCV/DeteccaoFaces/Exercicio1/cascades/haarcascade_frontalface_default.xml'
+dir = os.path.dirname(os.path.abspath(__file__))
+caminho_detector = os.path.join(dir,'./cascades/haarcascade_frontalface_default.xml')
 detector_face = cv2.CascadeClassifier(caminho_detector)
 
 #recebendo dados da webcam:
@@ -14,10 +14,10 @@ while True:
     ok, frame = video_capture.read()
 
     imagem_cinza = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    deteccoes = detector_face.detectMultiScale(imagem_cinza,minNeighbors=2)
+    deteccoes = detector_face.detectMultiScale(imagem_cinza,minNeighbors=3,minSize=(70,60))
     for(x,y,w,h) in deteccoes:
         print(w,h)
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,155,122),5)
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,205,255),5)
     cv2.imshow('Janela',frame)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
